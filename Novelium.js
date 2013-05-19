dfdca958b7d9371351e55baadcabc3676de451d0
s0-ma <tatsuya.soma@gmail.com>
2013-05-20 00:07:55 +0900

    Novelium-base class is modified.

--- Novelium.js
+++ Novelium.js
@@ -26,45 +26,55 @@ var Nv_Init = function(){
 	nv_window.setFlexibleWindow();
 };
 
-var Nv = (function(){
-	this.isBrowser = false;
-	this.browserName = "";
-	this.isTiApp = false;
-    this.isTideSDK = false;
-    this.isAdobeAir = false;
 
-	//プラットフォーム判別
-	if(navigator.userAgent.indexOf("Titanium") != -1){
-		this.isTiApp = true;
-	}else if(navigator.userAgent.indexOf("tide-sdk") != -1){
-        this.isTideSDK = true;
-	}else if(navigator.userAgent.indexOf("AdobeAIR") != -1){
-        this.isAdobeAir = true;
-	}else{
-		this.isBrowser = true;
-		//ブラウザ判別
-		var userAgent = window.navigator.userAgent.toLowerCase();
-		if (userAgent.indexOf('opera') != -1) {
-			this.browserName = 'opera';
-		} else if (userAgent.indexOf('msie') != -1) {
-			this.brouserName = 'ie';
-		} else if (userAgent.indexOf('chrome') != -1) {
-			this.browserName = 'chrome';
-		} else if (userAgent.indexOf('safari') != -1) {
-			this.browserName = 'safari';
-		} else if (userAgent.indexOf('gecko') != -1) {
-			this.browserName = 'gecko';
-		} else {
-			this.browserName = 'unknown';
-		}
-	}
+if(!window.Nv){
+    var Nv = (function(){
+    
+        //Constructor
+        function Nv(){
+            //Property
+    	    this.isBrowser = false;
+    	    this.browserName = "";
+    	    this.isTiApp = false;
+            this.isTideSDK = false;
+            this.isAdobeAir = false;
+    
+    	    //プラットフォーム判別
+    	    if(navigator.userAgent.indexOf("Titanium") != -1){
+    	    	this.isTiApp = true;
+    	    }else if(navigator.userAgent.indexOf("tide-sdk") != -1){
+                this.isTideSDK = true;
+    	    }else if(navigator.userAgent.indexOf("AdobeAIR") != -1){
+                this.isAdobeAir = true;
+    	    }else{
+    	    	this.isBrowser = true;
+    	    	//ブラウザ判別
+    	    	var userAgent = window.navigator.userAgent.toLowerCase();
+    	    	if (userAgent.indexOf('opera') != -1) {
+    	    		this.browserName = 'opera';
+    	    	} else if (userAgent.indexOf('msie') != -1) {
+    	    		this.brouserName = 'ie';
+    	    	} else if (userAgent.indexOf('chrome') != -1) {
+    	    		this.browserName = 'chrome';
+    	    	} else if (userAgent.indexOf('safari') != -1) {
+    	    		this.browserName = 'safari';
+    	    	} else if (userAgent.indexOf('gecko') != -1) {
+    	    		this.browserName = 'gecko';
+    	    	} else {
+    	    		this.browserName = 'unknown';
+    	    	}
+    	    }
+    
+            //HTML5対応状況確認
+            if(!document.createElement('canvas').getContext){
+                alert("This browser seems not to support HTML5(canvas)!")
+            }else if(!document.createElement('audio').canPlayType){
+                alert("This browser seems not to support HTML5(audio)!")
+            }
+    
+            return this;
+        }
 
-    //HTML5対応状況確認
-    if(!document.createElement('canvas').getContext){
-        alert("This browser seems not to support HTML5(canvas)!")
-    }else if(!document.createElement('audio').canPlayType){
-        alert("This browser seems not to support HTML5(audio)!")
-    }
-
-	return this;
-})();
+    	return new Nv();
+    })();
+}
